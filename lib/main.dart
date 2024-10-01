87a2d3c1bd90fb9f5f57ddadc2a6c43dfa2d1292
bryanica <bicaro@unicesar.edu.co>
2024-10-01 07:38:46 -0500

    add changes and new dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:prodtrack/pages/index_pages.dart';
 import 'package:prodtrack/pages/login_page.dart';
 
 import 'firebase_options.dart';
 
 void main() async {
-  runApp(const MyApp());
+  // Asegúrate de que los widgets se han inicializado antes de llamar a Firebase.
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Espera a que Firebase se inicialice antes de continuar.
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // Ahora que Firebase está inicializado, puedes ejecutar la aplicación.
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -18,11 +25,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        title: "Mi application",
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: LoginPage());
+      title: "Mi aplicación",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: indexPages(), // La página principal de tu aplicación.
+    );
   }
 }
